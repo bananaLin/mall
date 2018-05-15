499ef033a7c2fff1c314c9d8bd984800c4c2e79f
junior <527929821@qq.com>
2018-05-15 22:06:19 +0800

    更新代码

--- src/main/java/com/mmall/controller/app/ShippingController.java
+++ src/main/java/com/mmall/controller/app/ShippingController.java
@@ -1,4 +1,18 @@
 package com.mmall.controller.app;
 
+@Controller
+@RequestMapping("/app/shipping")
 public class ShippingController {
+
+    @Autowrite
+    ShippingService shippingService;
+  
+    @RequestMapping("/add.do")
+    public Msg add(HttpSession httpSession, Shipping shpping){
+        if(shpping == null){
+            return null;
+        }
+        boolean result = shippingService.add(shpping);
+        return null;
+    }
 }
